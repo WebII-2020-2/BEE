2f25ab25e1c044fdeef202db35d8e5d1bcda8141
marciosamuel <marciosamuel12@outlook.com>
2021-05-19 18:37:29 -0300

    finalização da estrutura base

--- SPA/src/pages/Home/index.js
+++ SPA/src/pages/Home/index.js
@@ -1,14 +1,5 @@
 import React from 'react';
 import './Home.css';
-// import ExampleApiService from '../../services/api/ExampleApiService'
-
-// async function exampleApi() {
-//   const response = await ExampleApiService.getById(5);
-
-//   if (response.data.result) {
-//     // tal tal
-//   }
-// }
 
 function Home() {
   return <h1 className="home title">Página inicial apenas como teste :)</h1>;
