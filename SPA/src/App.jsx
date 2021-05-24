1f4e5086aa1eecac51e849839e1a430d218eb1da
marciosamuel <marciosamuel12@outlook.com>
2021-05-24 13:46:51 -0300

    alterando js para jsx e importando css do bootstrap

--- SPA/src/App.jsx
+++ SPA/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './routes';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 const App = () => (
