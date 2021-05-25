daf1373ba0e5bdee786c011a027d9d2fc4ee3cf2
marciosamuel <marciosamuel12@outlook.com>
2021-05-24 22:59:53 -0300

    adição de rota para dashboard

--- SPA/src/routes.js
+++ SPA/src/routes.js
@@ -4,7 +4,7 @@ import {
   Switch,
   Route,
 } from 'react-router-dom';
-
+import Dashboard from './pages/Dashboard';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
 
@@ -12,6 +12,9 @@ const Routes = () => (
   <Switch>
     <Route exact path="/">
       <Home />
+    </Route>
+    <Route path="/dashboard">
+      <Dashboard />
     </Route>
     <Route path="/not-found">
       <NotFound />
