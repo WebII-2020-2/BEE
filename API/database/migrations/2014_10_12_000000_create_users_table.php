1ab153b4c6224e8d415845f4377120c597a3a738
Claudio Henrique <HacskBoy@hotmail.com>
2021-05-24 23:01:22 -0300

    refactor: add lenght of attributes

--- API/database/migrations/2014_10_12_000000_create_users_table.php
+++ API/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,13 +15,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('cpf')->nullable();
-            $table->string('email')->unique();
+            $table->string('name',100);
+            $table->string('cpf',11)->nullable();
+            $table->string('email',100)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->integer('level_access')->default(1);
             $table->date('birth_date')->nullable();
-            $table->string('phone')->nullable();
+            $table->string('phone',14)->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
