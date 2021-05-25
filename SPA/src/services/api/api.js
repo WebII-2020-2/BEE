b7b8ef08c1d3a05a538c3dfb502cf7eea27ef830
Mateus Gomes <mateus.gs1999@gmail.com>
2021-05-25 19:53:41 -0300

    adicionado token na api

--- SPA/src/services/api/api.js
+++ SPA/src/services/api/api.js
@@ -1,7 +1,17 @@
 import axios from 'axios';
+import { getToken } from '../validation/auth';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL,
+  baseURL: 'http://api-bee.herokuapp.com/api',
+});
+
+api.interceptors.request.use(async (config) => {
+  const token = getToken();
+  if (token) {
+    // eslint-disable-next-line no-param-reassign
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
 
 export default api;
