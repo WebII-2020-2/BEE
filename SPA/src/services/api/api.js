1086e1317a854acd044517980f4c5f3b557c13fa
marciosamuel <marciosamuel12@outlook.com>
2021-05-25 21:27:19 -0300

    novas regras do eslint

--- SPA/src/services/api/api.js
+++ SPA/src/services/api/api.js
@@ -8,7 +8,6 @@ const api = axios.create({
 api.interceptors.request.use(async (config) => {
   const token = getToken();
   if (token) {
-    // eslint-disable-next-line no-param-reassign
     config.headers.Authorization = `Bearer ${token}`;
   }
   return config;
