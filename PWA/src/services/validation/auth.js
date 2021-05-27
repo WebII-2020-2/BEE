3d491bc33a9acd3137ea594f1178f326df2e19f4
marciosamuel <marciosamuel12@outlook.com>
2021-05-27 16:13:03 -0300

    criação de validação de token

--- PWA/src/services/validation/auth.js
+++ PWA/src/services/validation/auth.js
@@ -1,15 +1,18 @@
-import { verify } from 'jsonwebtoken';
+import nJwt from 'njwt';
 
 export const TOKEN_KEY = '@admin-Token';
 export const getToken = () => localStorage.getItem(TOKEN_KEY);
 
 export const isAuthenticated = () => {
-  let isValid = false;
-  verify(getToken(), process.env.REACT_APP_JWT_SECRET, ((err) => {
-    isValid = !err;
-    console.log(err);
-  }));
-  return isValid;
+  try {
+    return nJwt.verify(getToken(), process.env.REACT_APP_JWT_SECRET);
+  } catch ({ message }) {
+    if (message === 'Jwt not active') {
+      return true;
+    }
+    console.error(`TOKEN-ERROR: ${message}`);
+    return false;
+  }
 };
 
 export const loginAdmin = (token) => {
