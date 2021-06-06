ebf069191d91e8f696f74fda96b300f0de67a592
marciosamuel <marciosamuel12@outlook.com>
2021-06-06 20:36:38 -0300

    Ajuste de rota

--- PWA/src/pages/Home/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './Home.css';
-
-function Home() {
-  return <h1 className="home title">Página inicial apenas como teste :)</h1>;
-}
-
-export default Home;
---
+++ PWA/src/pages/HomeAdmin/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Image } from 'react-bootstrap';
+import logoBlack from '../../assets/img/bee-logo-black.svg';
+import AdminContainer from '../../components/AdminContainer';
+import './HomeAdmin.css';
+
+function HomeAdmin() {
+  return (
+    <AdminContainer link="Home">
+      <Image src={logoBlack} className="admin-main-content-logo" />
+    </AdminContainer>
+  );
+}
+
+export default HomeAdmin;
