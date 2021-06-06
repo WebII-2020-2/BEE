49e94e929a3f2fae79abec8b2bc4fbc7204270d4
marciosamuel <marciosamuel12@outlook.com>
2021-06-06 20:37:28 -0300

    ajuste na sidebar e novo componente de container

--- PWA/src/components/SidebarAdmin/index.jsx
+++ PWA/src/components/SidebarAdmin/index.jsx
@@ -3,11 +3,13 @@ import { Nav, Col, Image } from 'react-bootstrap';
 import { ArrowLeftCircle, LogOut } from 'react-feather';
 import { useHistory } from 'react-router-dom';
 import { logout } from '../../services/validation/auth';
+import logoColorful from '../../assets/img/bee-logo-color.svg';
+import user from '../../assets/img/default-user.png';
 import './SidebarAdmin.css';
 
 function SidebarAdmin(props) {
   const {
-    logo, avatar, disabled, click,
+    disabled, click, link,
   } = props;
 
   const history = useHistory();
@@ -20,10 +22,10 @@ function SidebarAdmin(props) {
   return (
     <Col sm md={4} lg={2} className={`admin-sidebar wide ${disabled && 'disabled'}`}>
       <Nav className="admin-sidebar">
-        <Image src={logo} className="admin-sidebar-logo" />
+        <Image src={logoColorful} className="admin-sidebar-logo" />
 
         <div className="admin-sidebar-user">
-          <Image src={avatar} className="admin-sidebar-user-avatar" rounded />
+          <Image src={user} className="admin-sidebar-user-avatar" rounded />
 
           <span className="admin-sidebar-user-info">
             <p className="admin-sidebar-user-info-name">Fabio Banana</p>
@@ -39,12 +41,11 @@ function SidebarAdmin(props) {
           Navegação
         </span>
 
-        <Nav.Link className="admin-sidebar-link selected" disabled href="/admin/produtos">Produtos</Nav.Link>
-        <Nav.Link className="admin-sidebar-link" href="/admin/categorias">Categorias</Nav.Link>
-        <Nav.Link className="admin-sidebar-link" href="/admin/promoções">Promoções</Nav.Link>
-        <Nav.Link className="admin-sidebar-link" href="/admin/vendas">Vendas</Nav.Link>
-        <Nav.Link className="admin-sidebar-link" href="/admin/envios">Envios</Nav.Link>
-        <Nav.Link className="admin-sidebar-link" href="/admin/relatorios">Relatórios</Nav.Link>
+        <Nav.Link className={`admin-sidebar-link ${link === 'produtos' && 'selected'}`} disabled={link === 'produtos'} href="/admin/produtos">Produtos</Nav.Link>
+        <Nav.Link className={`admin-sidebar-link ${link === 'categorias' && 'selected'}`} disabled={link === 'categorias'} href="/admin/categorias">Categorias</Nav.Link>
+        <Nav.Link className={`admin-sidebar-link ${link === 'promocoes' && 'selected'}`} disabled={link === 'promocoes'} href="/admin/promoções">Promoções</Nav.Link>
+        <Nav.Link className={`admin-sidebar-link ${link === 'vendas' && 'selected'}`} disabled={link === 'vendas'} href="/admin/vendas">Vendas</Nav.Link>
+        <Nav.Link className={`admin-sidebar-link ${link === 'relatorios' && 'selected'}`} disabled={link === 'relatorios'} href="/admin/relatorios">Relatórios</Nav.Link>
 
         <span className="admin-sidebar-close">
           <ArrowLeftCircle size={30} onClick={click} />
