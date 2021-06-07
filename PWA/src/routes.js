37bc398913cdbdc98a42ff9cb533d10538ae18a0
Mateus Gomes <mateus.gs1999@gmail.com>
2021-06-07 11:41:09 -0300

    adicionado rota da categoria admin

--- PWA/src/routes.js
+++ PWA/src/routes.js
@@ -8,6 +8,7 @@ import HomeAdmin from './pages/HomeAdmin';
 import NotFound from './pages/NotFound';
 import LoginAdmin from './pages/LoginAdmin';
 import ProductsAdmin from './pages/ProductsAdmin';
+import CategoriesAdmin from './pages/CategoriesAdmin';
 import { isAuthenticated } from './services/validation/auth';
 
 const PrivateRouteAdmin = ({ component: Component, ...rest }) => (
@@ -25,6 +26,7 @@ const Routes = () => (
   <Switch>
     <PrivateRouteAdmin path="/admin/home" component={HomeAdmin} />
     <PrivateRouteAdmin path="/admin/produtos" component={ProductsAdmin} />
+    <PrivateRouteAdmin path="/admin/categorias" component={CategoriesAdmin} />
     <Route path="/login-admin" component={LoginAdmin} />
     <Route path="/not-found" component={NotFound} />
     <Redirect path="*" to="/not-found" />
