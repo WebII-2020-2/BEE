26ad759854533666d107a082e39b0d6ccbb6cd07
Mateus Gomes <mateus.gs1999@gmail.com>
2021-06-08 14:16:24 -0300

    pagina NotFound estilizado

--- PWA/src/pages/NotFound/index.jsx
+++ PWA/src/pages/NotFound/index.jsx
@@ -4,11 +4,11 @@ import './NotFound.css';
 function NotFound() {
   return (
     <div className="not-found">
-      <h1 className="text">
+      <h1 className="not-found-text">
         404
       </h1>
-      <h2 className="text">
-        NOT FOUND
+      <h2 className="not-found-text">
+        Página não encontrada :(
       </h2>
     </div>
   );
