5e21e55763da14665e53f4dda00fa9ccfb44d5bd
marciosamuel <marciosamuel12@outlook.com>
2021-06-08 21:46:58 -0300

    Componente de card do produto e tela de listagem

--- PWA/src/pages/ProductsAdmin/ProductsList/index.jsx
+++ PWA/src/pages/ProductsAdmin/ProductsList/index.jsx
@@ -1,20 +1,23 @@
 import React from 'react';
 import { Row } from 'react-bootstrap';
 import AdminContainer from '../../../components/AdminContainer';
-import CardProdutoAdmin from '../../../components/ProdutoAdmin';
-import imagemProduto from '../../../assets/img/mock-data-product.svg';
+import CardProdutoAdmin from '../../../components/CardProdutoAdmin';
+import ProdutoAdminApiService from '../../../services/api/ProductAdminApiService';
 import './ProductsList.css';
 
 function ProductsList() {
+  const products = ProdutoAdminApiService.getAll();
   return (
     <AdminContainer link="produtos">
-      <Row className="product-list-admin">
-        <CardProdutoAdmin image={imagemProduto} name="Amendoim" price="40,00" />
-        <CardProdutoAdmin image={imagemProduto} name="Amendoim" price="40,00" />
-        <CardProdutoAdmin image={imagemProduto} name="Amendoim" price="40,00" />
-        <CardProdutoAdmin image={imagemProduto} name="Amendoim" price="40,00" />
-        <CardProdutoAdmin image={imagemProduto} name="Amendoim" price="40,00" />
-        <CardProdutoAdmin image={imagemProduto} name="Amendoim" price="40,00" />
+      <Row className="actions-product-list admin" />
+      <Row className="product-list admin">
+        {
+          products.map(({
+            id, image, name, price,
+          }) => (
+            <CardProdutoAdmin id={id} key={id} image={image} name={name} price={price.toFixed(2).toString().replace('.', ',')} />
+          ))
+        }
       </Row>
     </AdminContainer>
   );
