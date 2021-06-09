eb555eab24cdc478b38288d561e3b950cf814ccf
Mateus Gomes <mateus.gs1999@gmail.com>
2021-06-09 19:57:51 -0300

    adicionado funcao para transformar a linha da tabela clicável

--- PWA/src/components/TableListAdmin/index.jsx
+++ PWA/src/components/TableListAdmin/index.jsx
@@ -1,24 +1,32 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+
 import './TableListAdmin.css';
 
 function TableList(props) {
-  const { itens, tablehead } = props;
+  const { itens, tableHead, linkEdit } = props;
 
-  const th = () => Object.values(tablehead).map((thValue) => (<th key={thValue}>{thValue}</th>));
+  const history = useHistory();
+
+  const handleCLickEdit = (id) => {
+    history.push(`${linkEdit}/${id}`);
+  };
+
+  const th = () => Object.values(tableHead).map((thValue) => (<th key={thValue}>{thValue}</th>));
 
   const filterTd = (row) => {
-    const td = Object.entries(row).filter((item) => item[0] in tablehead);
+    const td = Object.entries(row).filter((item) => item[0] in tableHead);
     return td.map((t) => <td key={t[0]}>{t[1]}</td>);
   };
 
   const tr = () => itens.map((row) => (
-    <tr key={row.id}>
+    <tr key={row.id} onClick={() => handleCLickEdit(row.id)}>
       {filterTd(row)}
     </tr>
   ));
 
   return (
-    <table className="w-100 table table-list">
+    <table className="w-100 table table-list table-hover">
       <thead>
         {th()}
       </thead>
