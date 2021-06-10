a87ed7197484a6ab87089b9792edc24ed9594ae3
Mateus Gomes <mateus.gs1999@gmail.com>
2021-06-09 21:39:45 -0300

    paginacao adicionado

--- PWA/src/pages/CategoriesAdmin/CategoriesList/index.jsx
+++ PWA/src/pages/CategoriesAdmin/CategoriesList/index.jsx
@@ -1,17 +1,26 @@
 import React, { useEffect, useState } from 'react';
 import AdminContainer from '../../../components/AdminContainer';
 import ButtonsListAdmin from '../../../components/ButtonsListAdmin';
+import PaginationAdmin from '../../../components/PaginationAdmin';
 import TableListAdmin from '../../../components/TableListAdmin';
 import CategoryAdminApiService from '../../../services/api/CategoryAdminApiService';
 
 function CategoriesList() {
   const [categories, setCategories] = useState();
+  const [actualPage, setActualPage] = useState(1);
+
   const th = { id: 'ID', name: 'Nome' };
+  let size = 1;
+
+  const handleChangePage = (value) => {
+    setActualPage(value);
+  };
 
   useEffect(() => {
     try {
       const response = CategoryAdminApiService.getAll();
       setCategories(response);
+      size = categories.length;
     } catch (e) {
       console.log(e);
     }
@@ -20,7 +29,16 @@ function CategoriesList() {
   return (
     <AdminContainer link="categorias">
       <ButtonsListAdmin link="/admin/categorias/novo" />
-      <TableListAdmin itens={categories || []} tableHead={th} linkEdit="/admin/categorias" />
+      <TableListAdmin
+        itens={categories || []}
+        tableHead={th}
+        linkEdit="/admin/categorias"
+      />
+      <PaginationAdmin
+        itensCount={size}
+        actualPage={actualPage}
+        click={handleChangePage}
+      />
     </AdminContainer>
   );
 }
