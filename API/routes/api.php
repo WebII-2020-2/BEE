ab2d80d6ffaafa659ca5a942870de2e003f76f94
Claudio Henrique <HacskBoy@hotmail.com>
2021-06-16 19:31:16 -0300

    refactor[api]: increments cors and add routes to get by id

--- API/routes/api.php
+++ API/routes/api.php
@@ -3,6 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+header("access-control-allow-origin: https://localhost:3000");
+header("access-control-allow-origin: http://localhost:3000");
+header("access-control-allow-origin: https://bee-web2.vercel.app");
+
 // Logon
 Route::group(['middleware' => 'api'], function ($router) {
     Route::post('register', 'Api\UserController@register');
@@ -20,6 +24,7 @@ Route::group(['middleware' => ['jwt.verify'], 'prefix' => 'card'], function ($ro
 Route::group(['middleware' => ['jwt.verify'], 'prefix' => 'product'], function ($router) {
     Route::post('/add', 'Api\ProductController@store');
     Route::get('/list', 'Api\ProductController@show');
+    Route::get('/list/{id}', 'Api\ProductController@get');
     Route::post('/update', 'Api\ProductController@update');
     Route::post('/delete', 'Api\ProductController@delete');
 });
@@ -28,6 +33,7 @@ Route::group(['middleware' => ['jwt.verify'], 'prefix' => 'product'], function (
 Route::group(['middleware' => ['jwt.verify'], 'prefix' => 'category'], function ($router) {
     Route::post('/add', 'Api\CategoryController@store');
     Route::get('/list', 'Api\CategoryController@show');
+    Route::get('/list/{id}', 'Api\CategoryController@get');
     Route::post('/update', 'Api\CategoryController@update');
     Route::post('/delete', 'Api\CategoryController@delete');
 });
