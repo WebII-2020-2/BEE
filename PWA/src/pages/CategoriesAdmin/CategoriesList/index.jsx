d15433f7d039cc4f7a69ff5d72f285a4d3aa158b
Mateus Gomes <mateus.gs1999@gmail.com>
2021-06-16 23:08:17 -0300

    adicionado animação de loading e paginação

--- PWA/src/pages/CategoriesAdmin/CategoriesList/index.jsx
+++ PWA/src/pages/CategoriesAdmin/CategoriesList/index.jsx
@@ -3,21 +3,22 @@ import AdminContainer from '../../../components/AdminContainer';
 import ButtonsListAdmin from '../../../components/ButtonsListAdmin';
 import PaginationAdmin from '../../../components/PaginationAdmin';
 import TableListAdmin from '../../../components/TableListAdmin';
+import LoadingPageAdmin from '../../../components/LoadingPageAdmin';
 import CategoryAdminApiService from '../../../services/api/CategoryAdminApiService';
 
-function CategoriesList() {
+function CategoriesList(props) {
+  const { match } = props;
   const [categories, setCategories] = useState([]);
+  const [categoriesPerPage, setCategoriesPerPage] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   const [actualPage, setActualPage] = useState(1);
+  const totalPages = Math.ceil(categories.length / 5);
 
   const th = { id: 'ID', name: 'Nome' };
-  const size = 1;
-
-  const handleChangePage = (value) => {
-    setActualPage(value);
-  };
 
   const getCategories = async () => {
     try {
+      setIsLoading(true);
       const resp = await CategoryAdminApiService.getAll().then((r) => r.data);
       if (resp.success) {
         setCategories(resp.data);
@@ -26,6 +27,8 @@ function CategoriesList() {
       }
     } catch (err) {
       console.error(err);
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -33,18 +36,43 @@ function CategoriesList() {
     getCategories();
   }, []);
 
+  const getCategoriesPerPage = () => {
+    const indexMin = (actualPage - 1) * 5;
+    const indexMax = indexMin + 5;
+    const categoryList = categories.filter(
+      (x, index) => index >= indexMin && index < indexMax
+    );
+    setCategoriesPerPage(categoryList);
+  };
+
+  useEffect(() => {
+    if (match.params.number) {
+      setActualPage(Number(match.params.number));
+    }
+    getCategoriesPerPage();
+  }, [categories, actualPage]);
+
+  const handleChangePage = (page) => {
+    setActualPage(page);
+  };
+
   return (
     <AdminContainer link="categorias">
       <ButtonsListAdmin link="/admin/categorias/novo" />
-      <TableListAdmin
-        itens={categories}
-        tableHead={th}
-        linkEdit="/admin/categorias"
-      />
+      {isLoading ? (
+        <LoadingPageAdmin />
+      ) : (
+        <TableListAdmin
+          itens={categoriesPerPage}
+          tableHead={th}
+          linkEdit="/admin/categorias"
+        />
+      )}
       <PaginationAdmin
-        itensCount={size}
+        totalPages={totalPages}
         actualPage={actualPage}
-        click={handleChangePage}
+        changePage={handleChangePage}
+        baseUrl="/admin/categorias"
       />
     </AdminContainer>
   );
