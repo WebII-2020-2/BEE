74f18afca7b85c417b95ff9a79985f84355b499e
Mateus Gomes <mateus.gs1999@gmail.com>
2021-06-17 21:19:52 -0300

    base das paginas de promoções criada

---
+++ PWA/src/pages/PromotionsAdmin/PromotionsList/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AdminContainer from '../../../components/AdminContainer';
+
+function PromotionsList() {
+  return (
+    <AdminContainer link="promocoes">
+      <h1>Lista das promoções</h1>
+    </AdminContainer>
+  );
+}
+
+export default PromotionsList;
---
+++ PWA/src/pages/PromotionsAdmin/PromotionsNew/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AdminContainer from '../../../components/AdminContainer';
+
+function PromotionsNew() {
+  return (
+    <AdminContainer link="promocoes">
+      <h1>Cadastro de promoções</h1>
+    </AdminContainer>
+  );
+}
+
+export default PromotionsNew;
