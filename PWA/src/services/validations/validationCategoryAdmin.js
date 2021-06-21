0550d1420a3d748dde489a1436877b100ab779e0
Mateus Gomes <mateus.gs1999@gmail.com>
2021-06-21 00:36:31 -0300

    validação de tamanho maximo para nome adicionado

--- PWA/src/services/validations/validationCategoryAdmin.js
+++ PWA/src/services/validations/validationCategoryAdmin.js
@@ -1,7 +1,11 @@
 import * as yup from 'yup';
 
 const validationSchema = yup.object().shape({
-  name: yup.string().required('Campo obrigatório: Nome').nullable(false),
+  name: yup
+    .string()
+    .required('Campo obrigatório: Nome')
+    .nullable(false)
+    .max(100, 'Nome deve conter menos de 100 caracteres'),
   description: yup
     .string()
     .required('Campo obrigatório: Descrição')
