fe6b3d18289d769a9071bb523fdd4e4c6f9b9212
Mateus Gomes <mateus.gs1999@gmail.com>
2021-06-22 03:13:55 -0300

    adicionado promocoes para test

--- PWA/src/services/api/PromotionAdminApiService.js
+++ PWA/src/services/api/PromotionAdminApiService.js
@@ -1,15 +1,33 @@
 // import api from './api';
 
 // const ENDPOINT_BASE = '/category';
-
+const promocoes = [
+  {
+    id: 1,
+    name: 'Dia das mães',
+    start_date: '2020-06-12',
+    end_date: '2020-07-12',
+    type: 1,
+    value: 15,
+  },
+  {
+    id: 2,
+    name: 'Natal',
+    start_date: '2021-12-25',
+    end_date: '2020-12-30',
+    type: 2,
+    value: 20,
+  },
+];
 /** traz todos */
 function getAll() {
   // return api.get(`${ENDPOINT_BASE}/list`);
+  return promocoes;
 }
 
 /** busca por ID */
 function getById(id) {
-  console.warn(id);
+  return promocoes.filter((e) => e.id === id);
   // return api.get(`${ENDPOINT_BASE}/list/${id}`);
 }
 
