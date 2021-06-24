496ee1963885928b805de95076313cd1a825bf4a
marciosamuel <marciosamuel12@outlook.com>
2021-06-24 11:51:23 -0300

    Divisão de abas em componentes

---
+++ PWA/src/pages/OrderAdmin/OrderPage/Tabs/index.jsx
@@ -0,0 +1,10 @@
+import Details from './Details';
+import Products from './Products';
+import Shipping from './Shipping';
+import './Tabs.css';
+
+export default {
+  Details,
+  Products,
+  Shipping,
+};
