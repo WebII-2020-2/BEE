0aa738732e22ade46c8ff7f4b5a7489c6335d9dd
marciosamuel <marciosamuel12@outlook.com>
2021-06-24 11:56:13 -0300

    Nova page utilizando os componentes de relatorios

--- PWA/src/pages/ReportsAdmin/ReportsPage/index.jsx
+++ PWA/src/pages/ReportsAdmin/ReportsPage/index.jsx
@@ -1,16 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
 import { Container } from 'react-bootstrap';
-import {
-  AreaChart,
-  Area,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-  ResponsiveContainer,
-} from 'recharts';
 import AdminContainer from '../../../components/AdminContainer';
 import ButtonsFormAdmin from '../../../components/ButtonsFormAdmin';
+import ReportCardAdmin from '../../../components/ReportAdmin/ReportCard';
+import ReportGraphAdmin from '../../../components/ReportAdmin/ReportGraph';
 import ReportsAdminApiService from '../../../services/api/ReportsAdminApiService';
 import orderStatistcs from '../../../services/utils/orderStatistics';
 import './ReportsPage.css';
@@ -18,13 +11,6 @@ import './ReportsPage.css';
 function ReportsPage(props) {
   const { match } = props;
   const [reportData, setReportData] = useState([]);
-  const [statistics, setStatistics] = useState({
-    totalProducts: 0,
-    meanProducts: 0,
-    totalValue: 0,
-    meanValue: 0,
-    graphData: [],
-  });
 
   const getReports = async () => {
     try {
@@ -41,98 +27,61 @@ function ReportsPage(props) {
     }
   };
 
-  useEffect(() => {
-    getReports();
-  }, []);
-
-  useEffect(() => {
-    setStatistics({
+  const statistics = useMemo(
+    () => ({
       totalProducts: orderStatistcs.getTotalProducts(reportData),
       meanProducts: orderStatistcs.getMeanProducts(reportData),
       totalValue: orderStatistcs.getTotalValue(reportData),
       meanValue: orderStatistcs.getMeanValue(reportData),
       graphData: orderStatistcs.getGraphData(reportData, match.params.id),
-    });
-  }, [reportData]);
+    }),
+    [reportData]
+  );
+
+  useEffect(() => {
+    getReports();
+  }, []);
+
+  const valueData = [
+    {
+      title: 'Total das vendas do mês',
+      value: `R$ ${statistics.totalValue}`,
+    },
+    {
+      title: 'Valor médio das vendas p/ dia',
+      value: `R$ ${statistics.meanValue}`,
+    },
+  ];
+
+  const productsData = [
+    {
+      title: 'Total de produtos do mês',
+      value: `R$ ${statistics.totalProducts}`,
+    },
+    {
+      title: 'Média de produtos vendidos p/ dia',
+      value: `R$ ${statistics.meanProducts}`,
+    },
+  ];
 
   return (
     <AdminContainer link="relatorios">
       <ButtonsFormAdmin path="/admin/relatorios" isReadOnly />
       <Container className="report-page-admin container">
-        <div className="report-page-admin cards">
-          <div className="report-page-admin card">
-            <p>Total das vendas do mês</p>
-            <span>R$ {statistics.totalValue}</span>
-          </div>
-          <div className="report-page-admin card">
-            <p>Valor médio das vendas p/ dia</p>
-            <span>R$ {statistics.meanValue}</span>
-          </div>
-        </div>
-
-        <div className="report-page-admin graph">
-          <p className="report-page-admin title">Vendas do mês</p>
-
-          <ResponsiveContainer width="100%" height="80%">
-            <AreaChart width={500} height={200} data={statistics.graphData}>
-              <defs>
-                <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
-                  <stop offset="5%" stopColor="#f7ca00" stopOpacity={0.8} />
-                  <stop offset="95%" stopColor="#f7ca00" stopOpacity={0} />
-                </linearGradient>
-              </defs>
-              <XAxis dataKey="dia" />
-              <YAxis />
-              <CartesianGrid strokeDasharray="3 3" />
-              <Tooltip filterNull label="lalala" />
-              <Area
-                type="monotone"
-                dataKey="valor"
-                stroke="#fca311"
-                fillOpacity={1}
-                fill="url(#colorUv)"
-              />
-            </AreaChart>
-          </ResponsiveContainer>
-        </div>
-
-        <div className="report-page-admin cards">
-          <div className="report-page-admin card">
-            <p>Total de produtos do mês</p>
-            <span>{statistics.totalProducts}/un</span>
-          </div>
-          <div className="report-page-admin card">
-            <p>Média de produtos vendidos p/ dia</p>
-            <span>{statistics.meanProducts}/un</span>
-          </div>
-        </div>
-
-        <div className="report-page-admin graph">
-          <p className="report-page-admin title">
-            Quantidade de produtos vendidos
-          </p>
-          <ResponsiveContainer width="100%" height="80%">
-            <AreaChart width={500} height={200} data={statistics.graphData}>
-              <defs>
-                <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
-                  <stop offset="5%" stopColor="#f7ca00" stopOpacity={0.8} />
-                  <stop offset="95%" stopColor="#f7ca00" stopOpacity={0} />
-                </linearGradient>
-              </defs>
-              <XAxis dataKey="dia" />
-              <YAxis />
-              <CartesianGrid strokeDasharray="3 3" />
-              <Tooltip />
-              <Area
-                type="monotone"
-                dataKey="produtos"
-                stroke="#fca311"
-                fillOpacity={1}
-                fill="url(#colorUv)"
-              />
-            </AreaChart>
-          </ResponsiveContainer>
-        </div>
+        <ReportCardAdmin data={valueData} />
+        <ReportGraphAdmin
+          graphData={statistics.graphData}
+          areaKey="valor"
+          xKey="dia"
+          title="Vendas do mês"
+        />
+        <ReportCardAdmin data={productsData} />
+        <ReportGraphAdmin
+          graphData={statistics.graphData}
+          areaKey="produtos"
+          xKey="dia"
+          title="Quantidade de produtos vendidos"
+        />
       </Container>
     </AdminContainer>
   );
