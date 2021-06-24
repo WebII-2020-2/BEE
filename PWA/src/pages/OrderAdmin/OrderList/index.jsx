94d711f1b02b567172aadb033c567b548550fe02
marciosamuel <marciosamuel12@outlook.com>
2021-06-24 11:55:22 -0300

    Adição de formatadores e uso do useMemo

--- PWA/src/pages/OrderAdmin/OrderList/index.jsx
+++ PWA/src/pages/OrderAdmin/OrderList/index.jsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
+import { useHistory } from 'react-router-dom';
 import AdminContainer from '../../../components/AdminContainer';
 import ButtonsListAdmin from '../../../components/ButtonsListAdmin';
 import LoadingPageAdmin from '../../../components/LoadingPageAdmin';
@@ -6,15 +7,16 @@ import PaginationAdmin from '../../../components/PaginationAdmin';
 import TableListAdmin from '../../../components/TableListAdmin';
 import OrderAdminApiService from '../../../services/api/OrderAdminApiService';
 import orderStatus from '../../../services/utils/orderStatus';
+import formatFloat from '../../../services/utils/formatFloat';
+import formatDate from '../../../services/utils/formatDate';
 
 function OrderList(props) {
   const { match } = props;
+  const history = useHistory();
   const [isLoading, setIsLoading] = useState(true);
   const [orders, setOrders] = useState([]);
-  const [ordersFilter, setOrdersFilter] = useState([]);
-  const [ordersPerPage, setOrdersPerPage] = useState([]);
-  const [actualPage, setActualPage] = useState(1);
-  const [totalPages, setTotalPages] = useState(1);
+  const [valueSearch, setValueSearch] = useState();
+  const [actualPage, setActualPage] = useState();
 
   const th = {
     invoice: 'Nº Venda',
@@ -30,24 +32,12 @@ function OrderList(props) {
     try {
       const resp = await OrderAdminApiService.getAll().then((r) => r.data);
       if (resp.success) {
-        const formattedOrders = resp.data.map((order) => {
-          const selledDate = new Date(order.selled_date).toLocaleDateString(
-            'pt-BR',
-            {
-              timeZone: 'UTC',
-            }
-          );
-          return {
-            invoice: ' ',
-            ...order,
-            value_total: order.value_total
-              .toFixed(2)
-              .toString()
-              .replace('.', ','),
-            status_order: orderStatus.convert(order.status_order),
-            selled_date: selledDate,
-          };
-        });
+        const formattedOrders = resp.data.map((order) => ({
+          ...order,
+          value_total: `R$ ${formatFloat(order.value_total)}`,
+          status_order: orderStatus.convert(order.status_order),
+          selled_date: formatDate(order.selled_date),
+        }));
         setOrders(formattedOrders);
       } else {
         throw new Error(`${resp.error.error_message}`);
@@ -59,57 +49,46 @@ function OrderList(props) {
     }
   };
 
-  const getOrderFilter = (valueSearch) => {
+  const ordersFilter = useMemo(() => {
     const filter = valueSearch || undefined;
     if (filter) {
-      const dateFilter = new Date(filter).toLocaleDateString('pt-BR', {
-        timeZone: 'UTC',
-      });
-      const filtered = orders.filter(
-        (order) => order.selled_date === dateFilter
-      );
-      setOrdersFilter(filtered);
-    } else {
-      setOrdersFilter(-1);
+      const dateFilter = formatDate(filter);
+      return orders.filter((order) => order.selled_date === dateFilter);
     }
-  };
+    return -1;
+  }, [valueSearch, orders]);
 
-  const getOrdersPerPage = () => {
+  const totalPages = useMemo(() => {
+    if (ordersFilter !== -1) {
+      return Math.ceil(ordersFilter.length / 8);
+    }
+    return Math.ceil(orders.length / 8);
+  }, [orders, ordersFilter]);
+
+  const ordersPerPage = useMemo(() => {
     const indexMin = (actualPage - 1) * 8;
     const indexMax = indexMin + 8;
     if (ordersFilter !== -1) {
-      const productList = ordersFilter.filter(
+      return ordersFilter.filter(
         (x, index) => index >= indexMin && index < indexMax
       );
-      setOrdersPerPage(productList);
-      setTotalPages(Math.ceil(ordersFilter.length / 8));
-    } else {
-      const productList = orders.filter(
-        (x, index) => index >= indexMin && index < indexMax
-      );
-      setOrdersPerPage(productList);
-      setTotalPages(Math.ceil(orders.length / 8));
     }
-  };
-
-  const handleChangePage = (page) => {
-    setActualPage(page);
-  };
+    return orders.filter((x, index) => index >= indexMin && index < indexMax);
+  }, [actualPage, ordersFilter, orders]);
 
   useEffect(() => {
+    const page = Number(match.params.number);
+    if (page) {
+      setActualPage(page);
+    } else {
+      history.push('/admin/vendas/page/1');
+    }
     getOrder();
   }, []);
 
-  useEffect(() => {
-    if (match.params.number) {
-      setActualPage(Number(match.params.number));
-    }
-    getOrdersPerPage();
-  }, [orders, ordersFilter, actualPage, totalPages]);
-
   return (
     <AdminContainer link="vendas">
-      <ButtonsListAdmin funcFilter={getOrderFilter} filterType="date" />
+      <ButtonsListAdmin funcFilter={setValueSearch} filterType="date" />
       {isLoading ? (
         <LoadingPageAdmin />
       ) : (
@@ -122,7 +101,7 @@ function OrderList(props) {
       <PaginationAdmin
         totalPages={totalPages}
         actualPage={actualPage}
-        changePage={handleChangePage}
+        changePage={setActualPage}
         baseUrl="/admin/vendas"
       />
     </AdminContainer>
