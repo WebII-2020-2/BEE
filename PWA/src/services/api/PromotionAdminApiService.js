0f2c3037efd9723db891142aeab8c57eab58eced
Mateus Gomes <mateus.gs1999@gmail.com>
2021-06-29 22:18:03 -0300

    rotas da api atualizadas

--- PWA/src/services/api/PromotionAdminApiService.js
+++ PWA/src/services/api/PromotionAdminApiService.js
@@ -1,57 +1,34 @@
-// import api from './api';
+import api from './api';
 
-// const ENDPOINT_BASE = '/category';
-const promocoes = [
-  {
-    id: 1,
-    name: 'Dia das mães',
-    start_date: '2020-06-12',
-    end_date: '2020-07-12',
-    type: 1,
-    value: 15,
-  },
-  {
-    id: 2,
-    name: 'Natal',
-    start_date: '2021-12-25',
-    end_date: '2020-12-30',
-    type: 2,
-    value: 20,
-  },
-];
+const ENDPOINT_BASE = '/promotion';
+
 /** traz todos */
 function getAll() {
-  // return api.get(`${ENDPOINT_BASE}/list`);
-  return promocoes;
+  return api.get(`${ENDPOINT_BASE}/list`);
 }
 
 /** busca por ID */
 function getById(id) {
-  return promocoes.filter((e) => e.id === id);
-  // return api.get(`${ENDPOINT_BASE}/list/${id}`);
+  return api.get(`${ENDPOINT_BASE}/list/${id}`);
 }
 
 /** cria um novo */
 function createNew(form) {
-  console.warn(form);
-  // return api.post(`${ENDPOINT_BASE}/add`, {
-  //   ...form,
-  // });
+  return api.post(`${ENDPOINT_BASE}/add`, {
+    ...form,
+  });
 }
 
 /** deleta */
 function remove(id) {
-  console.warn(id);
-  // return api.post(`${ENDPOINT_BASE}/delete/${id}`);
+  return api.post(`${ENDPOINT_BASE}/delete/${id}`);
 }
 
 /** atualiza */
 function update(form, id) {
-  console.warn(id);
-  console.warn(form);
-  // return api.post(`${ENDPOINT_BASE}/update/${id}`, {
-  //   ...form,
-  // });
+  return api.post(`${ENDPOINT_BASE}/update/${id}`, {
+    ...form,
+  });
 }
 
 const CategoryAdminApiService = {
