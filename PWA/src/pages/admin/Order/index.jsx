30149d53300b662734f2b198297d8b4d236b1565
marciosamuel <marciosamuel12@outlook.com>
2021-06-30 22:56:10 -0300

    Correção do redirect para a página Not-Found

--- PWA/src/pages/admin/Order/index.jsx
+++ PWA/src/pages/admin/Order/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
 // PAGES
 import OrderList from './OrderList';
@@ -12,6 +12,7 @@ function Order(props) {
     <Switch>
       <Route path={`${match.path}/page/:number`} component={OrderList} />
       <Route path={`${match.path}/:id`} component={OrderPage} />
+      <Redirect path="*" to="/not-found" />
     </Switch>
   );
 }
--- PWA/src/pages/admin/Reports/index.jsx
+++ PWA/src/pages/admin/Reports/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
 // PAGES
 import ReportsList from './ReportsList';
@@ -12,6 +12,7 @@ function Reports(props) {
     <Switch>
       <Route path={`${match.path}/page/:number`} component={ReportsList} />
       <Route path={`${match.path}/:id`} component={ReportsPage} />
+      <Redirect path="*" to="/not-found" />
     </Switch>
   );
 }
