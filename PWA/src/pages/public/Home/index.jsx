a270a492562307b9d24f6010b39c5dbb7054fb53
marciosamuel <marciosamuel12@outlook.com>
2021-07-05 11:35:46 -0300

    Adição de título e criação do arquivo de css

--- PWA/src/pages/public/Home/index.jsx
+++ PWA/src/pages/public/Home/index.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import StoreContainer from '../../../components/Shared/StoreContainer';
 
 function Home() {
+  document.title = 'BEE - Página Inicial';
+
   return (
     <StoreContainer>
       <h1>Página Inicial</h1>
