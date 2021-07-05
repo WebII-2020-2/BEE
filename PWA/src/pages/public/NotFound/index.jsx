33c11390d6403195f3bfce1ca9567a650e9c56db
marciosamuel <marciosamuel12@outlook.com>
2021-07-05 15:30:26 -0300

    Utilização do store container e estrutura inicial

--- PWA/src/pages/public/NotFound/index.jsx
+++ PWA/src/pages/public/NotFound/index.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import './NotFound.css';
 import img404 from '../../../assets/img/404.png';
+import StoreContainer from '../../../components/Shared/StoreContainer';
 
 function NotFound() {
   return (
-    <div className="not-found-container">
-      <h1 className="not-found-text">Oops...</h1>
-      <img src={img404} alt="broken-page" className="not-found-image" />
-      <h2 className="not-found-text">
-        A página que você estava procurando não está aqui
-      </h2>
-    </div>
+    <StoreContainer title="Página não encontrada">
+      <div className="not-found-container">
+        <h1 className="not-found-text">Oops...</h1>
+        <img src={img404} alt="broken-page" className="not-found-image" />
+        <h2 className="not-found-text">
+          A página que você estava procurando não está aqui
+        </h2>
+      </div>
+    </StoreContainer>
   );
 }
 
---
+++ PWA/src/pages/public/Promotions/PromotionsList/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import StoreContainer from '../../../../components/Shared/StoreContainer';
+
+function PromotionsList() {
+  return (
+    <StoreContainer title="todas as promocoes">
+      Lista de Promoções
+    </StoreContainer>
+  );
+}
+
+export default PromotionsList;
