a362876e4bd82f3b967a0acc69d562a10a1d073d
marciosamuel <marciosamuel12@outlook.com>
2021-07-07 14:39:43 -0300

    Página de categoria com spinner e estrutura base

--- PWA/src/pages/public/Category/index.jsx
+++ PWA/src/pages/public/Category/index.jsx
@@ -1,15 +1,60 @@
-import React from 'react';
-import { Container } from 'react-bootstrap';
+import React, { useEffect, useState } from 'react';
+import { Container, Spinner } from 'react-bootstrap';
 import StoreContainer from '../../../components/Shared/StoreContainer';
+import CategoryAdminApiService from '../../../services/api/CategoryAdminApiService';
 
 function CategoryPage(props) {
   const { match } = props;
+  const [categoryData, setCategoryData] = useState({
+    name: '',
+  });
+  const [loadingData, setLoadingData] = useState(true);
+
+  const getCategoryData = async () => {
+    setLoadingData(true);
+    try {
+      const resp = await CategoryAdminApiService.getById(match.params.id)
+        .then((r) => r.data)
+        .catch((r) => r.response.data);
+
+      if (resp.success) {
+        setCategoryData(resp.data);
+      } else {
+        throw resp.error;
+      }
+    } catch (err) {
+      console.error(`ERRO ${err.code}: ${err.error_message}`);
+    } finally {
+      setLoadingData(false);
+    }
+  };
+
+  useEffect(() => {
+    getCategoryData();
+  }, []);
 
   return (
-    <StoreContainer title={match.params.id}>
+    <StoreContainer title={categoryData.name}>
       <Container className="products-container">
         <div className="actions">Ações</div>
         <div className="products">Produtos</div>
+        {loadingData ? (
+          <Spinner
+            variant="secondary"
+            animation="border"
+            style={{
+              height: 64,
+              width: 64,
+              alignSelf: 'center',
+              margin: '1rem',
+            }}
+          />
+        ) : (
+          <>
+            <h1>{categoryData.name}</h1>
+            <p>{categoryData.description}</p>
+          </>
+        )}
       </Container>
     </StoreContainer>
   );
