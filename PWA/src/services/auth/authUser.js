b36a1dffb337ac4cfbd367320e214e12daac555a
Mateus Gomes <mateus.gs1999@gmail.com>
2021-07-10 11:13:05 -0300

    autenticacao do usuario

---
+++ PWA/src/services/auth/authUser.js
@@ -0,0 +1,24 @@
+import nJwt from 'njwt';
+
+export const TOKEN_KEY = '@user-Token';
+export const getToken = () => localStorage.getItem(TOKEN_KEY);
+
+export const isAuthenticated = () => {
+  try {
+    return nJwt.verify(getToken(), process.env.REACT_APP_JWT_SECRET);
+  } catch ({ message }) {
+    if (message === 'Jwt not active') {
+      return true;
+    }
+    console.error(`TOKEN-ERROR: ${message}`);
+    return false;
+  }
+};
+
+export const loginUser = (token) => {
+  localStorage.setItem(TOKEN_KEY, token);
+};
+
+export const logout = () => {
+  localStorage.removeItem(TOKEN_KEY);
+};
