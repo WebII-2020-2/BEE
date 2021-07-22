12857827135a60e3a11322dd646439b3a601b6e2
Claudio Henrique <HacskBoy@hotmail.com>
2021-07-21 21:58:34 -0300

    feat[api]: increment forgot password

---
+++ API/app/Mail/SendMailForgotPassword.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class SendMailForgotPassword extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($data)
+    {
+        $this->email = $data['email'];
+        $this->token = $data['token'];
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->markdown('Mail.send_mail_forgot_password', [
+            'messages' => 'Clique no link abaixo para trocar a sua senha. Se você recebeu isso e não solicitou a troca de senha, ignore esse e-mail.',
+            'link' => 'https://bee-web2.vercel.app/forgot/password/'.$this->token])
+                ->to($this->email)
+                ->from('HacskBoy@hotmail.com')
+                ->subject('Solicitação de troca de senha.');
+    }
+}
---
+++ API/resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,5 @@
+<tr>
+    <td class="header">
+        <img src="{{ asset('logo-dark.svg') }}" class="logo" style="width: 10rem;" alt="Logo do BEE">
+    </td>
+</tr>
