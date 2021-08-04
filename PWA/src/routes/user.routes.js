2f9a36cef9f766217585261fe1c3c3a639c5679c
Mateus Gomes <mateus.gs1999@gmail.com>
2021-08-04 00:31:10 -0300

    rota de dashboard privata para depois do login

--- PWA/src/routes/user.routes.js
+++ PWA/src/routes/user.routes.js
@@ -1,7 +1,21 @@
 import React from 'react';
 import { Redirect, Switch, Route } from 'react-router-dom';
-
+import { isAuthenticated } from '../services/auth/authUser';
 import Logon from '../pages/user/Logon';
+import Dashboard from '../pages/user/Dashboard';
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={(props) =>
+      isAuthenticated() ? (
+        <Component {...props} />
+      ) : (
+        <Redirect to="/user/login" {...props} />
+      )
+    }
+  />
+);
 
 function UserRoutes(props) {
   const { match } = props;
@@ -9,6 +23,10 @@ function UserRoutes(props) {
   return (
     <Switch>
       <Route path={`${match.path}/login`} component={Logon} />
+      <PrivateRoute
+        path={`${match.path}/dashboard/:page`}
+        component={Dashboard}
+      />
       <Redirect path="*" to="/not-found" />
     </Switch>
   );
