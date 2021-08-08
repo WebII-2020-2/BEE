b438268fdc3c717a8518c1601d25a2a8a14de091
Mateus Gomes <mateus.gs1999@gmail.com>
2021-08-08 16:00:47 -0300

    pagina de adicionar cartao

---
+++ PWA/src/pages/user/Dashboard/pages/Cards/CardsNew/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import FormCard from '../../../../../../components/Shared/FormCard';
+
+function CardsNew() {
+  return (
+    <div className="container-dashboard">
+      <h2>Novo cartão</h2>
+      <FormCard />
+    </div>
+  );
+}
+
+export default CardsNew;
