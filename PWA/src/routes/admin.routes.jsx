3c3006058eb76c2f1b1d0245ecee8e9db9fc44d9
Marcio Samuel <marciosamuel12@outlook.com>
2021-08-08 18:11:01 -0300

    Alteração na extensão dos arquivos js para jsx

--- PWA/src/routes/admin.routes.jsx
+++ PWA/src/routes/admin.routes.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Redirect, Switch, Route } from 'react-router-dom';
+import { Redirect, Route, Switch } from 'react-router-dom';
+import Campaign from '../pages/admin/Campaigns';
 import Categories from '../pages/admin/Categories';
 import Home from '../pages/admin/Home';
 import Login from '../pages/admin/Login';
-import Order from '../pages/admin/Order';
+import Order from '../pages/admin/Orders';
 import Products from '../pages/admin/Products';
 import Promotions from '../pages/admin/Promotions';
 import Reports from '../pages/admin/Reports';
@@ -27,13 +28,20 @@ function AdminRoutes(props) {
 
   return (
     <Switch>
-      <Route path={`${match.path}/login`} component={Login} />
+      <Route path={`${match.path}/login`}>
+        {isAuthenticated() ? (
+          <Redirect to={`${match.path}/inicio`} />
+        ) : (
+          <Login />
+        )}
+      </Route>
       <PrivateRoute path={`${match.path}/inicio`} component={Home} />
       <PrivateRoute path={`${match.path}/categorias`} component={Categories} />
       <PrivateRoute path={`${match.path}/produtos`} component={Products} />
       <PrivateRoute path={`${match.path}/promocoes`} component={Promotions} />
       <PrivateRoute path={`${match.path}/relatorios`} component={Reports} />
       <PrivateRoute path={`${match.path}/vendas`} component={Order} />
+      <PrivateRoute path={`${match.path}/campanhas`} component={Campaign} />
       <Redirect path="*" to="/not-found" />
     </Switch>
   );
