bce1c936f81723f52cfd261faad1969992dd3381
Mateus Gomes <mateus.gs1999@gmail.com>
2021-08-09 18:40:19 -0300

    pagina de edicao de enderecos

--- PWA/src/pages/user/Dashboard/pages/Address/AddressPage/index.jsx
+++ PWA/src/pages/user/Dashboard/pages/Address/AddressPage/index.jsx
@@ -1,7 +1,86 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import FormAddress from '../../../../../../components/Shared/FormAddress';
+import AddressApiService from '../../../../../../services/api/AddressApiService';
 
-function AddressPage() {
-  return <div>Pagina Endereço</div>;
+function AddressPage(props) {
+  const { match } = props;
+  const history = useHistory();
+
+  const [values, setValues] = useState({});
+  const [isReadOnly, setIsReadOnly] = useState(true);
+  const [isSaving, setIsSaving] = useState(false);
+
+  const getAddress = async () => {
+    try {
+      const resp = await AddressApiService.getById(Number(match.params.id))
+        .then((r) => r.data)
+        .catch((r) => {
+          throw r.response.data.error;
+        });
+      if (resp.success) {
+        setValues(resp.data);
+      }
+    } catch (err) {
+      console.error(`ERRO ${err.code}: ${err.error_message}`);
+      history.push('/user/dashboard/enderecos');
+    }
+  };
+
+  const handleEdit = () => {
+    setIsReadOnly(!isReadOnly);
+  };
+
+  const handleSubmit = async (form) => {
+    try {
+      setIsSaving(true);
+      const resp = await AddressApiService.update(values.id, form)
+        .then((r) => r.data)
+        .catch((r) => {
+          throw r.response.data.error;
+        });
+      if (resp.success) {
+        setValues(resp.data);
+        handleEdit();
+      }
+    } catch (err) {
+      console.error(`ERRO ${err.code}: ${err.error_message}`);
+    } finally {
+      setIsSaving(false);
+    }
+  };
+
+  const handleDelete = async () => {
+    try {
+      const resp = await AddressApiService.remove(values.id)
+        .then((r) => r.data)
+        .catch((r) => {
+          throw r.response.data.error;
+        });
+      if (resp.success) {
+        history.push('/user/dashboard/enderecos');
+      }
+    } catch (err) {
+      console.error(`ERRO ${err.code}: ${err.error_message}`);
+    }
+  };
+
+  useEffect(() => {
+    getAddress();
+  }, []);
+
+  return (
+    <div className="container-dashboard">
+      <FormAddress
+        valuesAddress={values}
+        handleSubmitAddress={handleSubmit}
+        handleDelete={handleDelete}
+        handleEdit={handleEdit}
+        isSaving={isSaving}
+        isReadOnly={isReadOnly}
+      />
+    </div>
+  );
 }
 
 export default AddressPage;
