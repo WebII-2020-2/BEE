a6dbc1cd6f797f68242bda5d948b8f64e9c8442f
Mateus Gomes <mateus.gs1999@gmail.com>
2021-08-09 18:40:03 -0300

    listagem de enderecos

--- PWA/src/pages/user/Dashboard/pages/Address/AddressList/index.jsx
+++ PWA/src/pages/user/Dashboard/pages/Address/AddressList/index.jsx
@@ -1,7 +1,106 @@
 import React from 'react';
+import { Button, Col, Row } from 'react-bootstrap';
+// import AddressApiService from '../../../../../../services/api/AddressApiService';
 
-function AddressList() {
-  return <div>Pagina Cartão</div>;
+function AddressList(props) {
+  const { match } = props;
+  const valuesTest = [
+    {
+      id: '1',
+      public_place: 'Rua das bananas1',
+      district: 'Bananal',
+      number: '10',
+      complement: 'Banana',
+      zip_code: '10101012',
+      city: 'Bananal',
+      state: 'Ba',
+      reference_point: 'Bananui1',
+    },
+    {
+      id: '2',
+      public_place: 'Rua das bananas2',
+      district: 'Bananal',
+      number: '10',
+      complement: 'Banana',
+      zip_code: '10101011',
+      city: 'Bananal',
+      state: 'Ba',
+      reference_point: 'Bananui',
+    },
+  ];
+  // const [values, setValues] = useState([]);
+  // const [isLoading, setIsLoading] = useState(true);
+
+  // const getAddress = async () => {
+  //   try {
+  //     setIsLoading(true);
+  //     const resp = await AddressApiService.getAll()
+  //       .then((r) => r.data)
+  //       .catch((r) => {
+  //         throw r.response.data.error;
+  //       });
+  //     if (resp.success) {
+  //       setValues(resp.data);
+  //     }
+  //   } catch (err) {
+  //     console.error(`ERRO ${err.code}: ${err.error_message}`);
+  //   } finally {
+  //     setIsLoading(false);
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   getAddress();
+  // }, []);
+
+  const cardAddress = (value) => (
+    <div className="card-address-dashboard" key={value.id}>
+      <Row>
+        <Col>
+          <b>Logradouro</b> <br /> {`${value.public_place}, ${value.number}`}
+        </Col>
+        <Col>
+          <b>CEP</b> <br />
+          {`${value.zip_code.substr(0, 5)}-${value.zip_code.substr(5)}`}
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <b>Bairro</b> <br /> {value.district}
+        </Col>
+        <Col>
+          <b>Cidade / Estado</b> <br /> {`${value.city} / ${value.state}`}
+        </Col>
+      </Row>
+      {value.complement && (
+        <Row>
+          <Col>
+            <b>Complemento</b> <br /> {value.complement}
+          </Col>
+        </Row>
+      )}
+      <Row>
+        <Col className="edit-address-dashboard">
+          <Button variant="warning" href={`${match.url}/${value.id}`}>
+            Editar
+          </Button>
+        </Col>
+      </Row>
+    </div>
+  );
+
+  return (
+    <div className="container-dashboard">
+      <Button
+        variant="dark"
+        href={`${match.url}/novo`}
+        className="align-self-center"
+      >
+        Cadastrar novo endereço
+      </Button>
+      {valuesTest.map((value) => cardAddress(value))}
+    </div>
+  );
 }
 
 export default AddressList;
