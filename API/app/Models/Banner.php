5ea1522020abb7f4ab9ff36a63d4ac9ec9667ef6
aldeirnohan <aldeir_nohan03@live.com>
2021-08-10 14:20:22 -0300

    Update Banner.php

--- API/app/Models/Banner.php
+++ API/app/Models/Banner.php
@@ -17,6 +17,6 @@ class Banner extends Model
     protected $table = 'banners';
 
     public function bannerProduct(){
-        return $this->hasMany('App\Models\BannerProduct');
+        return $this->hasMany('App\Models\BannerProduct', 'banner_id');
     }
 }
