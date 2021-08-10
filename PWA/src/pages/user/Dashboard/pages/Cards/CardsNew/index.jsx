a73e6b69b96969dcf93f48983ce2c4d1c291fa02
Mateus Gomes <mateus.gs1999@gmail.com>
2021-08-10 00:51:07 -0300

    valores iniciais dos cartões

--- PWA/src/pages/user/Dashboard/pages/Cards/CardsNew/index.jsx
+++ PWA/src/pages/user/Dashboard/pages/Cards/CardsNew/index.jsx
@@ -2,10 +2,18 @@ import React from 'react';
 import FormCard from '../../../../../../components/Shared/FormCard';
 
 function CardsNew() {
+  const initialValuesCard = {
+    number: '',
+    flag: '',
+    security_code: '',
+    expiration_date: '',
+    holder: '',
+  };
+
   return (
     <div className="container-dashboard">
       <h2>Novo cartão</h2>
-      <FormCard />
+      <FormCard valuesCard={initialValuesCard} isNew />
     </div>
   );
 }
