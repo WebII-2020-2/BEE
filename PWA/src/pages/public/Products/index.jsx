10e742732a838eb4578316eedbc2e6b4c2c82b7d
Mateus Gomes <mateus.gs1999@gmail.com>
2021-08-11 01:08:24 -0300

    pagina de listagem de todos produtos

---
+++ PWA/src/pages/public/Products/index.jsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from 'react';
+import { Container } from 'react-bootstrap';
+import ListProducts from '../../../components/Shared/ListProducts';
+import StoreContainer from '../../../components/Shared/StoreContainer';
+import ProductApiService from '../../../services/api/ProductAdminApiService';
+
+function Products() {
+  const [products, setProducts] = useState([]);
+  const [loadingData, setLoadingData] = useState(true);
+
+  const getProducts = async () => {
+    setLoadingData(true);
+    try {
+      const resp = await ProductApiService.getAll()
+        .then((r) => r.data)
+        .catch((r) => {
+          throw r.response.data.error;
+        });
+      if (resp.success) {
+        setProducts(resp.data);
+      }
+    } catch (err) {
+      console.error(`ERRO ${err.code}: ${err.error_message}`);
+    } finally {
+      setLoadingData(false);
+    }
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  return (
+    <StoreContainer title="Produtos">
+      <Container className="products-container">
+        <div className="category-info">
+          <h1>Produtos</h1>
+          <hr />
+        </div>
+        <ListProducts productsData={products} loadingData={loadingData} />
+      </Container>
+    </StoreContainer>
+  );
+}
+
+export default Products;
