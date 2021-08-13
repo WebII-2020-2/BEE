b3adf1dfbe6bbe71352e45e9a6ba2445d0f62549
Cláudio Henrique <HacskBoy@hotmail.com>
2021-08-13 19:35:18 -0300

    feat[api]: increments order

--- API/app/Models/User.php
+++ API/app/Models/User.php
@@ -41,7 +41,7 @@ class User extends Authenticatable implements JWTSubject
     }
 
     public function order(){
-        return $this->hasMany('App\Models\Orders');
+        return $this->hasMany('App\Models\Order');
     }
 
     public function shoppingCart(){
