11301a56616a7ddc969306ae44d528b5c749a0b3
Marcio Samuel <marciosamuel12@outlook.com>
2021-08-17 11:00:01 -0300

    Finalizado pagina de sobre

--- PWA/src/pages/public/About/index.jsx
+++ PWA/src/pages/public/About/index.jsx
@@ -1,19 +1,96 @@
 import React from 'react';
 import { Container, Image } from 'react-bootstrap';
+import { GitHub, Linkedin, Mail } from 'react-feather';
 import StoreContainer from '../../../components/Shared/StoreContainer';
 import './About.css';
 
-function Sobre() {
-  const userImages = {
-    aldeir: 'https://avatars.githubusercontent.com/u/47931404?v=4',
-    claudio: 'https://avatars1.githubusercontent.com/u/30199497?v=4',
-    marcio: 'https://avatars1.githubusercontent.com/u/43766556?v=4',
-    mateus: 'https://avatars.githubusercontent.com/u/61122185?v=4',
-  };
+const devDataBack = [
+  {
+    name: 'Aldeir Nohan',
+    image: 'https://avatars.githubusercontent.com/u/47931404?v=4',
+    role: 'Desenvolvedor Back-end',
+    email: 'aldeir_nohan03@live.com',
+    github: 'https://github.com/aldeirnohan',
+    linkedin:
+      'https://linkedin.com/in/aldeir-norran-carvalho-de-souza-62b909152',
+  },
+  {
+    name: 'Cláudio Henrique',
+    image: 'https://avatars1.githubusercontent.com/u/30199497?v=4',
+    role: 'Desenvolvedor Back-end',
+    email: 'claudio.henrique.fdasilva@gmail.com',
+    github: 'https://github.com/claudiohenriquefds',
+    linkedin: 'https://linkedin.com/in/claudio-henrique-a3119a134',
+  },
+];
 
+const devDataFront = [
+  {
+    name: 'Marcio Samuel',
+    image: 'https://avatars1.githubusercontent.com/u/43766556?v=4',
+    role: 'Desenvolvedor Front-end',
+    email: 'marciosamuel12@gmail.com',
+    github: 'https://github.com/marciosamuel',
+    linkedin: 'https://linkedin.com/in/marciosamuel',
+  },
+  {
+    name: 'Mateus Gomes',
+    image: 'https://avatars.githubusercontent.com/u/61122185?v=4',
+    role: 'Desenvolvedor Front-end',
+    email: 'mateu.gs1999@gmail.com',
+    github: 'https://github.com/mateusgs29',
+    linkedin: 'https://linkedin.com/in/mateus-gs',
+  },
+];
+
+function Sobre() {
   const DevCard = (props) => {
-    const { image } = props;
-    return <Image src={image} alt="ok mo" className="dev-card" />;
+    const { name, image, role, email, github, linkedin } = props;
+    return (
+      <div className="card-dev">
+        <Image src={image} alt={`foto do ${name}`} />
+        <div className="card-info">
+          <h3>{name}</h3>
+          <h5>{role}</h5>
+          <hr />
+          <ul>
+            <li>
+              <a
+                href={`mailto:${email}`}
+                title={email}
+                className="link-about"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <Mail /> Email
+              </a>
+            </li>
+            <li>
+              <a
+                href={linkedin}
+                title={linkedin}
+                className="link-about"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <Linkedin /> Linkedin
+              </a>
+            </li>
+            <li>
+              <a
+                href={github}
+                title={github}
+                className="link-about"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <GitHub /> Github
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    );
   };
 
   return (
@@ -35,7 +112,7 @@ function Sobre() {
           realizar uma compra e visualizar os detalhes dos pedidos efetuados.
         </p>
 
-        <div>
+        <div className="mb-3">
           <a
             className="link-about"
             href="https://github.com/WebII-2020-2/BEE"
@@ -46,7 +123,7 @@ function Sobre() {
           </a>
           <br />
           <a
-            className="link-about mb-2"
+            className="link-about"
             href="https://www.figma.com/file/9XUhANzTUsepjryBKGDBi0/BEE?node-id=0%3A1"
             target="_blank"
             rel="noreferrer"
@@ -54,10 +131,41 @@ function Sobre() {
             Identidade visual e protótipo no Figma
           </a>
         </div>
-
         <h2>O Desenvolvimento</h2>
+        <p>
+          A parte do back-end do projeto foi construída no modelo de API REST e
+          arquitetura MVC. Para isso, foram utilizados o framework Laravel e a
+          persistência dos dados feita com a ajuda do MySQL e Google Cloud.
+        </p>
+        <p>
+          O front-end foi foi produzido em forma de SPA com a biblioteca
+          React.JS e o framework Bootstrap. Para consumo dos dados da API foi
+          utilizado o Axios e Redux para persistência de dados pela aplicação.
+          Também foram utilizadas bibliotecas de validação e CPF e Cartão de
+          Crédito, além de consulta de endereço por CEP.
+        </p>
+        <p>
+          Para o deploy e testes, utilizou-se o Heroku para a API e o Vercel
+          para preview da SPA. O git e Github auxiliou no versionamento e
+          compartilhamento de código juntamente com o gitflow.
+        </p>
+        <p>
+          A metodologia de desenvolvimento utilizada foi a Kanban juntamente com
+          o quadro do Trello, no quadro foram dispostos os cards com as tarefas
+          a serem feitas e atribuídas a seus respectivos responsáves, podendo
+          acompanhar o andamento de cada fase do projeto.
+        </p>
         <h2>O Time</h2>
-        <DevCard image={userImages.marcio} />
+        <Container className="dev-info">
+          {devDataBack.map((dev) => (
+            <DevCard {...dev} key={dev.name} />
+          ))}
+        </Container>
+        <Container className="dev-info">
+          {devDataFront.map((dev) => (
+            <DevCard {...dev} key={dev.name} />
+          ))}
+        </Container>
       </Container>
     </StoreContainer>
   );
